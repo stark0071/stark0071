8cd2518603696081ae65a615dfe165ceef83c2b7
stark0071 <100837562+stark0071@users.noreply.github.com>
2022-03-03 15:15:27 +0530

    Create Songs.js

---
+++ Songs.js
@@ -0,0 +1,76 @@
+const Songs = [
+  {
+    id: 1,
+    favourite: false,
+    songName: "Face-Off",
+    artist: "Tech N9ne,Dwayne Johnson",
+    song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FTech%20N9ne%20%E2%80%93%20Face%20Off%20Ft.%20Dwayne%20Johnson.mp3?alt=media&token=a0223ff0-6c54-497f-9f01-97ffd94e5306",
+    imgScr: 
+    "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2Fface-off.jfif?alt=media&token=defbd67e-d038-4e50-83f4-2a098e500663",
+ },
+ {
+     id: 2,
+    favourite: false,
+    songName: "Gods Plan",
+    artist: "Drake",
+    song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FDrake-Gods-Plan-via-Naijafinix.com_.mp3?alt=media&token=8f483c73-3b36-42bb-b552-b6b41e6599f7",
+    imgScr: 
+    "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FDrake-God's-Plan.jpg?alt=media&token=2bc7d075-1d52-4c8f-8da4-d83b0a49e048",
+ },
+ {
+     id: 3,
+    favourite: false,
+    songName: "Fair Trade",
+    artist: "Drake",
+    song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FDrake-Ft-Travis-Scott-Fair-Trade-(TunezJam.com).mp3?alt=media&token=cb16dca0-6dd2-4a9f-afe1-213ef933e345",
+    imgScr: 
+    "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FDrake-Fair-Trade.jpg?alt=media&token=eeedc606-861d-4b76-ad42-a8cfaac864a2",
+ },
+ {
+     id: 4,
+    favourite: false,
+    songName: "Trampholine",
+    artist: "Zyan Malik",
+    song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FTrampoline%20-%20Zayn%20Malik-%20%5BMyMp3Bhojpuri.In%5D.mp3?alt=media&token=ad7cabfd-1b89-4f68-b221-aaff83015557",
+    imgScr: 
+    "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FTrampholine.jpg?alt=media&token=8fb49636-471e-4b3e-913f-21620e8603b9",
+ },
+ {
+ id: 5,
+ favourite: false,
+ songName: "Onna Come Up",
+ artist: "Lil Eazzyy",
+ song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FLil_Eazzyy_Ft_G_Herbo_-_Onna_Come_Up_Remix_.mp3?alt=media&token=7f4e170f-711c-41a4-8778-a02283a84e39",
+ imgScr: 
+ "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FOnna%20Come%20Up.jfif?alt=media&token=3fa50ac2-6bdc-4732-8a27-cf20a5b9a366",
+ },
+ {
+ id: 6,
+ favourite: false,
+ songName: "Invincible",
+ artist: "Pop Smoke",
+ song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FPop-Smoke-Invincible-via-Naijafinix.com_.mp3?alt=media&token=6c1e0fa7-baf4-48d0-981b-d5794ad4a9b5",
+ imgScr: 
+ "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FPop-Smoke%20Invincible.jpg?alt=media&token=b2e0a21f-963c-483c-8b23-6b1cbffcb667",
+ },
+ {
+ id: 7,
+ favourite: false,
+ songName: "XO Tour Life",
+ artist: "Lil Uzi Vert",
+ song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2FLil_Uzi_Vert_-_XO_Tour_Life.mp3?alt=media&token=6d83b767-8822-4e68-9301-52a9ca23eca8",
+ imgScr: 
+ "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FXo%20Tour%20Llif3.png?alt=media&token=57722a6d-a075-4431-ba3c-8a528f29cd43",
+  },
+  {
+  id: 8,
+  favourite: false,
+  songName: "Freaks",
+  artist: "Jordan Clarke",
+ song: "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/songs%2F%5BMP3DOWNLOAD.TO%5D%20Jordan%20Clarke%20-%20Freaks%20(Lyrics)-128k.mp3?alt=media&token=bd9cf966-cc10-4184-b41f-ad504b2d2416",
+ imgScr: 
+ "https://firebasestorage.googleapis.com/v0/b/my-aap-e27da.appspot.com/o/song%20images%2FFreaks.jpg?alt=media&token=9ce7e51e-cfe1-4187-8989-abd1fa77fefe",
+  },
+ ];
+ 
+ export { Songs };
